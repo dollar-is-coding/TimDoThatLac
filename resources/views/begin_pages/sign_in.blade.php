2210ec7941658aa89dd13fe829b437d385ce6a29
Dollar-is-studying <dollar.dev02@gmail.com>
2022-10-30 19:44:36 +0700

    sign in

--- resources/views/begin_pages/sign_in.blade.php
+++ resources/views/begin_pages/sign_in.blade.php
@@ -1,4 +1,11 @@
 @extends('index')
 @section('body')
-    
+    <form action="" method="post">
+        <input type="text" name="username" class="form-control" placeholder="Enter email or phone">
+        <input type="text" name="password" class="form-control" placeholder="Enter password">
+        <button type="submit" class="btn btn-primary">Sign In</button>
+        <div><a href="" >Forgot password?</a></div>
+        <hr>
+        <a href="" class="btn btn-success">Create your account</a>
+    </form>
 @endsection
