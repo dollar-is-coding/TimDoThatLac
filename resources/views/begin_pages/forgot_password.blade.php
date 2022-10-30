404717183e27b4e3b8e26958bae029b59379167d
Dollar-is-studying <dollar.dev02@gmail.com>
2022-10-30 22:19:35 +0700

    merge & fix

--- resources/views/begin_pages/forgot_password.blade.php
+++ resources/views/begin_pages/forgot_password.blade.php
@@ -1,11 +1,13 @@
-@extends('index')
-@section('body')
+@extends('begin')
+@section('begin')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <h2>Quên mật khẩu</h2>
-     <form class="form-lable" method="POST" action="..." >
-         <p>  <label class="control-label">Email:</label>            
-              <input class="form-control" name="email">     
-         </p>       
-         <p><button type="submit" class="btn btn-warning">Gửi mail</button></p>
+  <h3 class="text-center">Quên mật khẩu</h3>
+  <hr>
+     <form class="form-lable" method="POST" action="" >
+          <label class="control-label">Email</label>
+          <input type="email" class="form-control" name="email" placeholder="Email">     
+         <div class="text-center pt-3 pb-1">
+          <button type="submit" class="btn btn-success">Gửi mail</button>
+         </div>
      </form> 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NguoiDungController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('begin_pages/sign_in');
-});
+Route::get('/dang-ky',[NguoiDungController::class,'create'])->name('dang-ky');
 
