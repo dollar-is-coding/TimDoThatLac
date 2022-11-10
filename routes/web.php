86f2f82023fd293dfd883c291f7511c1031609cf
Dinh <0306201403@caothang.edu.vn>
2022-11-10 21:57:28 +0700

    pull

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,3 @@ Route::post('/dang-ky',[NguoiDungController::class,'store'])->name('xl-dang-ky')
 Route::get('/dang-nhap',[NguoiDungController::class,'sign_in'])->name('dang-nhap')->middleware('guest');
 Route::post('/dang-nhap',[NguoiDungController::class,'get_sign_in'])->name('xl-dang-nhap')->middleware('guest');
 Route::get('dangxuat',[NguoiDungController::class,'log_out'])->name('dang-xuat')->middleware('auth');
-
-Route::get('/1',function(){
-     return view('main_pages.new_feed_detail');
- });
