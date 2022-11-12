c6b6af1e9f7193436600bdf513a1ecb3278800eb
Dollar-is-studying <dollar.dev02@gmail.com>
2022-11-12 17:47:01 +0700

    checkout qua tk quy zinh

--- resources/views/main_pages/detail_post.blade.php
+++ resources/views/main_pages/detail_post.blade.php
@@ -1,89 +1,100 @@
 @extends('main')
 @section('main')
-<p>{{$soLuongHA}}</p>
-<div style="padding-left:20em;padding-right:20em;">
-    <div class="bg-light p-4 mt-3 mb-3 pt-3 pb-3 rounded-2 shadow-sm">
-        <div class="d-flex align-items-center justify-content-between">
-            <a href="">
-                <img src="{{ URL('images/avatar.jpg') }}" alt="img" width="50px" height="50px" class="rounded-circle mr-3">
-            </a>
-            <div class="d-flex align-content-center flex-column" style="padding-left:2%">
-                <div>
-                    <a href="" class="text-decoration-none fw-semibold" style="color:black">{{ $baiDang->nguoiDung->ho_ten }}</a>
+    <p>{{ $soLuongHA }}</p>
+    <div style="padding-left:20em;padding-right:20em;">
+        <div class="bg-light p-4 mt-3 mb-3 pt-3 pb-3 rounded-2 shadow-sm">
+            <div class="d-flex align-items-center justify-content-between">
+                <a href="">
+                    <img src="{{ URL('images/avatar.jpg') }}" alt="img" width="50px" height="50px"
+                        class="rounded-circle mr-3">
+                </a>
+                <div class="d-flex align-content-center flex-column" style="padding-left:2%">
+                    <div>
+                        <a href="" class="text-decoration-none fw-semibold"
+                            style="color:black">{{ $baiDang->nguoiDung->ho_ten }}</a>
+                    </div>
+                    <div style="color:rgb(154, 155, 157)">{{ $baiDang->created_at->format('d/m/Y') }}</div>
                 </div>
-                <div style="color:rgb(154, 155, 157)">{{ $baiDang->created_at->format('d/m/Y') }}</div>
+                @if ($baiDang->trang_thai == 1)
+                    <div class="col" style="text-align: right;">
+                        <div class="dropdown">
+                            <button class="btn btn-link" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="{{ URL('images/detail.png') }}" class="rounded-circle"
+                                    style="width: 2.3em;height:2.3em">
+                            </button>
+                            <ul class="dropdown-menu">
+                                @if (Auth::id() == $baiDang->nguoiDung->id)
+                                    <li>
+                                        <a class="dropdown-item"
+                                            href="{{ route('da-tim-thay', ['id' => $baiDang->id]) }}">{{ $baiDang->the_loai_id == 1 ? 'Đã trả' : 'Đã tìm thấy' }}</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item"
+                                            href="{{ route('chinh-sua-bai-dang', ['id' => $baiDang->id]) }}">Chỉnh sửa</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item"
+                                            href="{{ route('xoa-bai-dang', ['id' => $baiDang->id]) }}">Xoá</a>
+                                    </li>
+                                @else
+                                    <li><a class="dropdown-item" href="{{ Auth::id() == null ? '/dang-nhap' : '' }}">Theo
+                                            dõi</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="{{ Auth::id() == null ? '/dang-nhap' : '' }}">Báo
+                                            cáo</a>
+                                    </li>
+                                @endif
+                            </ul>
+                        </div>
+                    </div>
+                @else
+                    <div class="col" style="text-align: right;opacity:.3">
+                        <button class="btn btn-link" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="{{ URL('images/detail.png') }}" class="rounded-circle"
+                                style="width: 2.3em;height:2.3em">
+                        </button>
+                    </div>
+                @endif
             </div>
-            @if ($baiDang->trang_thai == 1)
-            <div class="col" style="text-align: right;">
-                <div class="dropdown">
-                    <button class="btn btn-link" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="{{ URL('images/detail.png') }}" class="rounded-circle" style="width: 2.3em;height:2.3em">
-                    </button>
-                    <ul class="dropdown-menu">
-                        @if (Auth::id() == $baiDang->nguoiDung->id)
-                        <li>
-                            <a class="dropdown-item" href="{{ route('da-tim-thay', ['id' => $baiDang->id]) }}">{{ $baiDang->the_loai_id == 1 ? 'Đã trả' : 'Đã tìm thấy' }}</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('chinh-sua-bai-dang', ['id' => $baiDang->id]) }}">Chỉnh sửa</a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('xoa-bai-dang', ['id' => $baiDang->id]) }}">Xoá</a>
-                        </li>
-                        @else
-                        <li><a class="dropdown-item" href="{{ Auth::id() == null ? '/dang-nhap' : '' }}">Theo
-                                dõi</a>
-                        </li>
-                        <li><a class="dropdown-item" href="{{ Auth::id() == null ? '/dang-nhap' : '' }}">Báo
-                                cáo</a>
-                        </li>
-                        @endif
-                    </ul>
+            <hr>
+            <div class=" d-flex flex-column justify-content-between">
+                <div class="fs-4 fw-semibold text-justify">{{ $baiDang->tieu_de }}</div>
+                <div class="d-flex text-center mt-2">
+                    @if ($baiDang->trang_thai == 0)
+                        <div class="rounded p-2 pt-0 pb-0 bg-success text-white">
+                            {{ $baiDang->the_loai_id == 1 ? 'Đã trả' : 'Đã tìm thấy' }}
+                        </div>
+                        &ensp;
+                    @else
+                        <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->theLoai->ten }}
+                        </div>
+                        &ensp;
+                    @endif
+                    <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->danhMuc->ten }}
+                    </div>
+                    &ensp;
+                    <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->khuVuc->ten }}</div>
                 </div>
+                <div class="mt-2">{{ $baiDang->noi_dung }}</div>
+                <div class="mt-2 mb-2">Địa chỉ: {{ $baiDang->dia_chi }}</div>
             </div>
-            @else
-            <div class="col" style="text-align: right;opacity:.3">
-                <button class="btn btn-link" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="{{ URL('images/detail.png') }}" class="rounded-circle" style="width: 2.3em;height:2.3em">
-                </button>
+            <div class="mt-3 mb-3">
+                @foreach ($hinhAnh as $item)
+                    <div class="border">
+                        <img src="{{ URL("images/$item->hinh_anh") }}" alt="" height="100%">
+                    </div>
+                @endforeach
             </div>
+            @if (Auth::id() != null)
+                <div class="input-group">
+                    <input class=" form-control rounded-5" type="text" name="" class="form-control"
+                        placeholder="Bình luận ...">&ensp;
+                    <div class="rounded-3" style="padding-left:10px">
+                        <button class="btn btn-outline-primary" type="submit" id="button-addon2"> <img
+                                src="{{ URL('images/send.png') }}" style="width:1.45em;"> </button>
+                    </div>
+                </div>
             @endif
         </div>
-        <hr>
-        <div class=" d-flex flex-column justify-content-between">
-            <div class="fs-4 fw-semibold text-justify">{{ $baiDang->tieu_de }}</div>
-            <div class="d-flex text-center mt-2">
-                @if ($baiDang->trang_thai == 0)
-                <div class="rounded p-2 pt-0 pb-0 bg-success text-white">
-                    {{ $baiDang->the_loai_id == 1 ? 'Đã trả' : 'Đã tìm thấy' }}
-                </div>
-                &ensp;
-                @else
-                <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->theLoai->ten }}
-                </div>
-                &ensp;
-                @endif
-                <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->danhMuc->ten }}
-                </div>
-                &ensp;
-                <div class="rounded p-2 pt-0 pb-0" style="background-color:#EEEEEE ;">{{ $baiDang->khuVuc->ten }}</div>
-            </div>
-            <div class="mt-2">{{ $baiDang->noi_dung }}</div>
-            <div class="mt-2 mb-2">Địa chỉ: {{ $baiDang->dia_chi }}</div>
-        </div>
-        <div class="mt-3 mb-3">
-
-
-
-        </div>
-        @if (Auth::id() != null)
-        <div class="input-group">
-            <input class=" form-control rounded-5" type="text" name="" class="form-control" placeholder="Bình luận ...">&ensp;
-            <div class="rounded-3" style="padding-left:10px">
-                <button class="btn btn-outline-primary" type="submit" id="button-addon2"> <img src="{{ URL('images/send.png') }}" style="width:1.45em;"> </button>
-            </div>
-        </div>
-        @endif
     </div>
-</div>
 @endsection
